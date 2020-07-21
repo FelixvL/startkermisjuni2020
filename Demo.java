d18ce05dd148b43ec1b58dedbb4faadac80c48f2
FelixvL <digitaleservice@gmail.com>
2020-07-21 16:22:34 +0200

    naar de methode

---
+++ Demo.java
@@ -0,0 +1,19 @@
+class Demo {
+	public static void main(String[] args) {
+		System.out.println("relaxt opstarten");
+		new Demo().programmabedienen();
+	}
+	void programmabedienen() {
+		System.out.println("Welke attractie wilt u bedienen?");
+	}
+}
+
+
+
+
+
+
+
+
+
+
