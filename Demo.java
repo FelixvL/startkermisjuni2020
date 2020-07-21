5ac46de18587df47f139e84ace4f6b3b773ba7df
FelixvL <digitaleservice@gmail.com>
2020-07-21 16:25:35 +0200

    de invoer wordt gelezen

--- Demo.java
+++ Demo.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 class Demo {
 	public static void main(String[] args) {
 		System.out.println("relaxt opstarten");
 		new Demo().programmabedienen();
+		System.out.println("we zijn klaar met het programma");
 	}
 	void programmabedienen() {
-		System.out.println("Welke attractie wilt u bedienen?");
+		System.out.println("Welke attractie wilt u een kaartje voor hebben?");
+		Scanner scanner = new Scanner(System.in);
+		String invoer = scanner.nextLine();		
+		System.out.println("u heeft gekozen voor: "+invoer);
 	}
 }
 
