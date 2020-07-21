bbc67d39fb1b3f0ad670369996b8b45a8ae3552e
FelixvL <digitaleservice@gmail.com>
2020-07-21 16:43:46 +0200

    Attractie als klasse aangemaakt

--- Demo.java
+++ Demo.java
@@ -6,40 +6,40 @@ class Demo {
 		new Demo().programmabedienen();
 		System.out.println("we zijn klaar met het programma");
 	}
+
 	void programmabedienen() {
 		Kassa kassa = new Kassa();
 		kassa.starten();
 	}
 }
 
-
-class Kassa{
-	Scanner scanner = new Scanner(System.in);	
-	void starten() {
-			boolean doorgaan = true;
-			while(doorgaan) {
-				String deKeuze = vraagOmAttractieKeuze();
-				if(deKeuze.equals("q")) {
-					doorgaan = false;
-				}else {
-					System.out.println("nog een rondje");
-				}
-			}
+class Attractie{
+	String naam;
+	void draaien() {
+		System.out.println("Ik ben aan het draaien, de attractie: "+ naam);
 	}
-	String vraagOmAttractieKeuze() {
-		System.out.println("Welke attractie wilt u een kaartje voor hebben?");
-		String invoer = scanner.nextLine();		
-		System.out.println("u heeft gekozen voor: "+invoer);
-		return invoer;
-	}
-	
 }
 
+class Kassa {
+	Scanner scanner = new Scanner(System.in);
+	Attractie a1 = new Attractie();
+		
+	void starten() {
+		boolean doorgaan = true;
+		while (doorgaan) {
+			String deKeuze = vraagOmAttractieKeuze();
+			if (deKeuze.equals("q")) {
+				doorgaan = false;
+			} else {
+				a1.draaien();
+			}
+		}
+	}
 
-
-
-
-
-
-
-
+	String vraagOmAttractieKeuze() {
+		System.out.println("Welke attractie wilt u een kaartje voor hebben?");
+		String invoer = scanner.nextLine();
+		System.out.println("u heeft gekozen voor: " + invoer);
+		return invoer;
+	}
+}
